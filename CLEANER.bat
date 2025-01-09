be5683ea3a53e7f8f98bdd7f75af78151a4efa52
esdragor <baudouinlapeze9@gmail.com>
2025-01-09 09:51:50 +0100

    create project

---
+++ CLEANER.bat
@@ -0,0 +1,10 @@
+del .vsconfig
+del *.sln
+del *.sln.DotSettings.user
+@RD /S /Q Binaries
+@RD /S /Q .vs
+@RD /S /Q .idea
+@RD /S /Q DerivedDataCache
+@RD /S /Q Intermediate
+@RD /S /Q Saved
+@RD /S /Q Build
